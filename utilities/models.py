993abc7d6f5cbf1964a71d4574e6e7ba5793b3e0
Nguyen Lan <lantrungsir28@gmail.com>
2018-03-10 22:40:10 +0700

    F

--- utilities/models.py
+++ utilities/models.py
@@ -3,9 +3,9 @@ config = {
   "apiKey": "AIzaSyB8Pcgzvzgn-LLP6Tccvq7MPgxHLiHzW2o",
   "authDomain": "anaconda-messenger.firebaseapp.com",
   "databaseURL": "https://anaconda-messenger.firebaseio.com",
+  "storageBucket": "anaconda-messenger.appspot.com",
   "serviceAccount": "./serviceAccountKey.json"
 }
-
 firebase = pyrebase.initialize_app(config)
 db = firebase.database()
 
