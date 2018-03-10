ad65187aacb21bef7ea171f3971ca324a3ef71e5
Nguyen Lan <lantrungsir28@gmail.com>
2018-03-10 22:47:55 +0700

    F

--- utilities/custom.py
+++ utilities/custom.py
@@ -2,12 +2,11 @@ import json
 from django.dispatch import receiver
 from allauth.account import signals
 from . import models
-from models import db
 def resolveMeetingData(request):
     if(request.method == "POST"):
         #GIVE ME STH
         meetingData = json.loads(request.body.decode('utf-8'))
-        db.child("meetings").set(meetingData)
+        models.db.child("meetings").set(meetingData)
         #save meetingData
 
 @receiver (signals.user_logged_in)
@@ -19,7 +18,7 @@ def savingUserData(sender, **kwargs):
         'name' : user.socialaccount_set.filter(provider = 'facebook')[0].extra_data['name'],
         'uid' : user.socialaccount_set.filter(provider = 'facebook')[0].extra_data['uid']
     }
-    db.child('users').set(userData)
+    models.db.child('users').set(userData)
 
 
 
