4f6b88a8ba43d887ff5a4d532f7f54611ea50776
Nguyen Lan <lantrungsir28@gmail.com>
2018-03-11 16:31:08 +0700

    F

--- utilities/custom.py
+++ utilities/custom.py
@@ -14,13 +14,13 @@ def getUserChatmates():
 def savingUserData(sender, **kwargs):
     user = kwargs.pop('user')
     #save user data
-    userData =  {
+    userData = {
         'profile_pic' : user.socialaccount_set.filter(provider = 'facebook')[0].get_avatar_url(),
         'name' : user.socialaccount_set.filter(provider = 'facebook')[0].extra_data['name']
     }    
     uid = user.socialaccount_set.filter(provider = 'facebook')[0].extra_data['id']
     
-    models.db.child('users/'.join(uid)).set(userData)
+    models.db.child('users/'+uid).set(userData)
 
 
 
