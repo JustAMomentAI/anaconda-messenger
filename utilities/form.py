caee28cd78f2d491603afd521f8dcc12ff1bdc48
Nguyen Lan <lantrungsir28@gmail.com>
2018-03-11 20:35:19 +0700

    F

--- utilities/form.py
+++ utilities/form.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class MeetingForm(forms.Form):
-    date = forms.DateField(input_formats= ('%Y-%m-%d',))
-    time = forms.TimeField(input_formats= ('%H:%M',))
-    content = forms.CharField(max_length = 1000)
-    note = forms.CharField(max_length = 2000)
+    date = forms.DateField(input_formats= ('%d/%m/%Y',), widget = forms.DateInput(attrs={"class":['w3-input', 'w3-padding-16']}))
+    time = forms.TimeField(input_formats= ('%H:%M',),widget = forms.TimeInput(attrs={"class" : ['w3-input', 'w3-padding-16']}))
+    content = forms.CharField(max_length = 1000, widget = forms.TextInput(attrs={"class": ['w3-input', 'w3-padding-16']}))
+    note = forms.CharField(max_length = 2000, widget = forms.TextInput(attrs={"class": ['w3-input', 'w3-padding-16']}))
