2f2772e9d646bb283f3d31eea3210eeb079067b3
Nguyen Lan <lantrungsir28@gmail.com>
2018-03-11 19:55:25 +0700

    F

--- utilities/form.py
+++ utilities/form.py
@@ -2,6 +2,6 @@ from django import forms
 
 class MeetingForm(forms.Form):
     date = forms.DateField(input_formats= ('%m/%d/%Y',))
-    time = forms.TimeField()
+    time = forms.TimeField(input_formats= ('%I:%M %p',))
     content = forms.CharField(max_length = 1000)
     note = forms.CharField(max_length = 2000)
