2948e4dab4b5d2d3a095650ab0382c7695560e51
Nguyen Lan <lantrungsir28@gmail.com>
2018-03-11 16:44:45 +0700

    F

--- utilities/static/custom-js/collect.js
+++ utilities/static/custom-js/collect.js
@@ -16,14 +16,14 @@ function setMeeting(){
     var date = document.getElementById("meeting-date").value;
     var time =  document.getElementById("meeting-time").value;
     var content = document.getElementById("meeting-content").value;
-    var note = document.getElementById("meeting-note").value;
+    var note = document.getElementById("meeting-notes").value;
     var xhr = new XMLHttpRequest();
     xhr.open("POST","/utilities/meetingset", true);
     xhr.setRequestHeader("Content-type", "application/json");
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
             var json = JSON.parse(xhr.responseText);
-            console.log(json.email + ", " + json.password);
+            console.log("success");
         }
     };
     var data = JSON.stringify({
