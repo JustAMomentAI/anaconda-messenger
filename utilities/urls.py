f5fb4210535ed2572c92420a7132e931ee1b6b8f
Nguyen Lan <lantrungsir28@gmail.com>
2018-03-11 16:50:29 +0700

    F

--- utilities/urls.py
+++ utilities/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     url(r'^$', views.loginPage, name = "index"),
     url(r'^home/$', views.homePage, name = "home"),
     #url(r'^messages/$', views.updateMessages, name = "message")
-    url(r'^meetings', views.goToSetMeetingsAndIdeas, name = "generate"),
-    url(r'meetingset', views.setMeeting, name = "meeting-set")
+    url(r'^meetings/$', views.goToSetMeetingsAndIdeas, name = "generate"),
+    url(r'^meetingset/$', views.setMeeting, name = "meeting-set")
 ]
